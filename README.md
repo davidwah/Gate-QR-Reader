fcdc12e119381cd1637d754b3f91959a8405deef
davidwah <davidwahyuyu@gmail.com>
2018-11-07 01:20:22 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Gate-QR-Reader
---
+++ setorDB.py
@@ -0,0 +1,58 @@
+import mysql.connector
+import json
+
+
+
+### Fungsi untuk kirim ke database ###
+
+### Gerbang Masuk ###
+def MQTT_Topic_GerbangMasuk(jsonData):
+    json_Dict = json.loads(jsonData)
+    dataMasuk = json_Dict['data_Masuk'] 
+        
+    # Koneksi dan kirim data ke Database
+    connection = mysql.connector.connect(host='192.168.1.100',
+                             database='testDB',
+                             user='david',
+                             password='admin')                 
+    mycursor = connection.cursor()
+
+    sql = "INSERT into gerbangMasuk (dataMasuk) values ('%s')" % dataMasuk
+    
+    mycursor.execute(sql)
+    connection.commit()
+    return
+
+    print "Input anda masuk pada database"
+    print ""
+
+
+### Gerbang Keluar ###
+def MQTT_Topic_GerbangKeluar(jsonData):
+    json_Dict = json.loads(jsonData)
+    dataKeluar = json_Dict['data_Keluar']
+
+    # Koneksi dan kirim data ke Database
+    connection = mysql.connector.connect(host='192.168.1.100',
+                             database='testDB',
+                             user='david',
+                             password='admin')                           
+    mycursor = connection.cursor()
+
+    sql = "INSERT into gerbangKeluar (dataKeluar) values ('%s')" % dataKeluar
+
+    mycursor.execute(sql)
+    connection.commit()
+    return
+
+    print "Input anda masuk pada database"
+    print ""
+
+
+def setor_Data(Topic, jsonData):
+    if Topic == "parkir/pintuMasuk":
+        # Parkir_GerbangMasuk(jsonData)
+        MQTT_Topic_GerbangMasuk(jsonData)
+    elif Topic == "parkir/pintuKeluar":
+        # Parkir_GerbangKeluar(jsonData)
+        MQTT_Topic_GerbangKeluar(jsonData)
