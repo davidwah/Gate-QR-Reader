97369381c15a749584b7debc6f63bdf6a9f32249
David Wahyu <22112331+davidwah@users.noreply.github.com>
2018-12-05 22:11:22 +0700

    Update setorDB.py

--- setorDB.py
+++ setorDB.py
@@ -1,20 +1,23 @@
 import mysql.connector
 import json
 
-### Fungsi untuk kirim ke database ###
+#######################################
+### Fungsi untuk kirim ke database 
+#######################################
+
 ### Gerbang Masuk ###
 def MQTT_Topic_GerbangMasuk(jsonData):
     json_Dict = json.loads(jsonData)
-    dataMasuk = json_Dict['data_Masuk'] 
-    
-    # Koneksi dan kirim data ke Database
-    connection = mysql.connector.connect(host='192.168.xxx.xxx',
-                             database='ooo',
-                             user='xxx',
-                             password='xxx')                 
+    username = json_Dict['data_Masuk'] 
+        
+    #Koneksi dan kirim data ke Database
+    connection = mysql.connector.connect(host='localhost',
+                             database='testDB',
+                             user='david',
+                             password='admin')                 
     mycursor = connection.cursor()
 
-    sql = "INSERT into gerbangMasuk (dataMasuk) values ('%s')" % dataMasuk
+    sql = "INSERT into gerbangMasuk (username) values ('%s')" % username
     
     mycursor.execute(sql)
     connection.commit()
@@ -27,29 +30,37 @@ def MQTT_Topic_GerbangMasuk(jsonData):
 ### Gerbang Keluar ###
 def MQTT_Topic_GerbangKeluar(jsonData):
     json_Dict = json.loads(jsonData)
-    dataKeluar = json_Dict['data_Keluar']
+    username = json_Dict['data_Keluar']
 
-    # Koneksi dan kirim data ke Database
-    connection = mysql.connector.connect(host='192.168.xxx.xxx',
-                             database='ooo',
-                             user='xxx',
-                             password='xxx')                           
-    mycursor = connection.cursor()
+    #Koneksi dan kirim data ke Database
+    connection = mysql.connector.connect(host='localhost',
+                             database='testDB',
+                             user='',
+                             password='')                           
+    mycursor = connection.cursor(True)
 
-    sql = "INSERT into gerbangKeluar (dataKeluar) values ('%s')" % dataKeluar
+    sql = "INSERT into gerbangKeluar (username) values ('%s')" % username
 
     mycursor.execute(sql)
     connection.commit()
-    return
+    # return
 
     print "Input anda masuk pada database"
+
+    sql_up = "UPDATE user set saldo = saldo - 5000 where user.username='%s' " % username
+    mycursor.execute(sql_up)
+    connection.commit()
+    # return
+
+    print "Input anda masuk pada database - Saldo berkurang"
     print ""
 
 
 def setor_Data(Topic, jsonData):
     if Topic == "parkir/pintuMasuk":
-        # Parkir_GerbangMasuk(jsonData)
+        #Parkir_GerbangMasuk(jsonData)
         MQTT_Topic_GerbangMasuk(jsonData)
     elif Topic == "parkir/pintuKeluar":
-        # Parkir_GerbangKeluar(jsonData)
+        #Parkir_GerbangKeluar(jsonData)
         MQTT_Topic_GerbangKeluar(jsonData)
+
