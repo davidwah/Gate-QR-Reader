08a1b0ee4da188a5f9bdcebc230216adddb92c6e
David Wahyu <22112331+davidwah@users.noreply.github.com>
2018-12-05 22:10:48 +0700

    Update subGerbang.py

--- subGerbang.py
+++ subGerbang.py
@@ -1,30 +1,29 @@
 import paho.mqtt.client as mqtt
 from setorDB import setor_Data
 
+
 ### Koneksi MQTT ###
-MQTT_Broker = "192.168.xxx.xxx"
+MQTT_Broker = "192.168.1.102"
 MQTT_Port   = 1883
 Keep_Alive_Interval = 45
 MQTT_Topic  = "parkir/#"
 
 def on_connect(self, mosq, obj, rc):
     self.subscribe(MQTT_Topic, 0)
-    #mqttc.subscribe(MQTT_Topic, 0)
-    #mqttc.connack_string(rc)
-    print("Connected with result code "+str(rc))
+    print("Connected with result code " + str(rc))
+    
 
 def on_message(mosq, obj, msg):
     print "MQTT Data diterima . . ."
     print "MQTT Topic: " + msg.topic
     print "Data: " + msg.payload
     print " "
-    setor_Data(msg.topic, msg.payload)
+    setor_Data(msg.topic, msg.payload) 
 
 def on_subcribe(mosq, obj, mid, grandted_qos):
     pass
 
 mqttc = mqtt.Client()
-
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 mqttc.on_subscribe = on_subcribe
@@ -33,7 +32,7 @@ mqttc.connect(MQTT_Broker, MQTT_Port, Keep_Alive_Interval)
 
 def publish_topic(topic, message):
     mqttc.publish(topic, message)
-    print("Terkirim: "+ str(message) + " " + "on MQTT Topic: " + str(topic))
+    print("Terkirim: " + str(message) + " " + "on MQTT Topic: " + str(topic))
     print " "
 
 mqttc.loop_forever()
